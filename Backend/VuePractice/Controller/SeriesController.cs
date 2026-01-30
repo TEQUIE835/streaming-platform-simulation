a0b3c7417407984d41fd9ca66708ece288f479df
TEQUIE835 <david123orjuela@gmail.com>
2026-01-30 07:05:39 -0500

    Finished Controllers

--- Backend/VuePractice/Controller/SeriesController.cs
+++ Backend/VuePractice/Controller/SeriesController.cs
@@ -1,6 +1,61 @@
+using _1.Application.DTOs.SeriesDtos;
+using _1.Application.Interfaces.SeriesInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace VuePractice.Controller;
 
-public class SeriesController
+[ApiController]
+[Authorize(Roles="Admin")]
+[Route("api/[controller]")]
+public class SeriesController : ControllerBase
 {
-    
+    private readonly ISeriesService _seriesService;
+
+    public SeriesController(ISeriesService seriesService)
+    {
+        _seriesService = seriesService;
+    }
+
+    [HttpPost("season/add")]
+    public async Task<IActionResult> AddSeasonAsync([FromBody] Guid seriesId)
+    {
+        try
+        {
+            await _seriesService.AddSeasonAsync(seriesId);
+            return Ok("Season Added");
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Error: " + e.Message);
+        }
+    }
+
+    [HttpPost("{seasonId}/episode/add")]
+    public async Task<IActionResult> AddEpisodeAsync(Guid seasonId,[FromBody] CreateEpisodeDto req)
+    {
+        try
+        {
+            await _seriesService.AddEpisodeAsync(seasonId, req);
+            return Ok("Episode Added");
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Error: " + e.Message);
+        }
+    }
+
+    [HttpPatch("episode/{id}/update")]
+    public async Task<IActionResult> UpdateEpisodeAsync(Guid id, [FromBody] UpdateEpisodeDto req)
+    {
+        try
+        {
+            await _seriesService.UpdateEpisodeAsync(id, req);
+            return Ok("Episode Updated");
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Error: " + e.Message);
+        }
+    }
 }
--- Backend/VuePractice/Controller/WatchHistoryController.cs
+++ Backend/VuePractice/Controller/WatchHistoryController.cs
@@ -1,6 +1,61 @@
+using _1.Application.DTOs.WatchHistoryDtos;
+using _1.Application.Interfaces.WatchHistoryInterfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 namespace VuePractice.Controller;
 
-public class WatchHistoryController
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class WatchHistoryController : ControllerBase
 {
-    
+    private readonly IWatchHistoryService _historyService;
+
+    public WatchHistoryController(IWatchHistoryService historyService)
+    {
+        _historyService = historyService;
+    }
+
+    [HttpPost("watch/{userId}/update")]
+    public async Task<IActionResult> UpdateProgressAsync(Guid userId, UpdateWatchProgressDto req)
+    {
+        try
+        {
+            await _historyService.UpdateProgressAsync(userId, req);
+            return Ok("Progress Updated");
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Error: " + e.Message);
+        }
+    }
+
+    [HttpGet("watchhistory/{userId}")]
+    public async Task<IActionResult> GetByUserAsync(Guid userId)
+    {
+        try
+        {
+            var res = await _historyService.GetByUserAsync(userId);
+            return Ok(res);
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Error: " + e.Message);
+        }
+    }
+
+    [HttpPut("watchhistory/{historyId}/complete")]
+    public async Task<IActionResult> CompleteWatchHistoryAsync(Guid historyId)
+    {
+        try
+        {
+            await _historyService.MarkCompletedAsync(historyId);
+            return Ok("Completed");
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Error: " + e.Message);
+        }
+    }
 }
